52f3513b779770c880bdcddf8ad6121699b6029a
rohith123-php <kokilamajunathan@gmail.com>
2026-06-05 12:58:41 +0530

    Add database connection configuration in db.php

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'school_db';
+$conn = new mysqli($host, $user, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
